e46237469115f9872dea3b862d56e3213759aaaf
Doctware <yusclever9@gmail.com>
2024-10-07 01:20:09 +0100

    the pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    this function returns a list of integers
+    representing a pascal triangle
+
+    if n <= 0 the return empty list
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # pascal triangle withh first row
+
+    # building each row of pascal triangle
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
