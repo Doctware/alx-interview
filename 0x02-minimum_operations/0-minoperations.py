d2b87b7f5eb85ececd05d36346c5938eb0bdaf73
Doctware <yusclever9@gmail.com>
2024-10-18 02:11:06 +0100

    the min operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+minOperations module
+
+This module contains a function `minOperations` that calculates the fewest
+number of operations (Copy All and Paste) required to achieve exactly n 'H'
+characters in a text file, starting with a single 'H'. If n is impossible
+to achieve, the function returns 0.
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations required to reach n 'H'
+    characters using Copy All and Paste operations
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
