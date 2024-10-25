0dc4953cac996301a5b51b904c6bab40ee15c987
Doctware <yusclever9@gmail.com>
2024-10-25 05:17:38 +0100

    generator

---
+++ 0x03-log_parsing/0-log_generator.py
@@ -0,0 +1,15 @@
+i#!/usr/bin/python3
+import random
+import sys
+from time import sleep
+import datetime
+
+for i in range(10000):
+    sleep(random.random())  # Simulates log creation delay
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),  # Random status codes
+        random.randint(1, 1024)  # Random file size
+    ))
+    sys.stdout.flush()
